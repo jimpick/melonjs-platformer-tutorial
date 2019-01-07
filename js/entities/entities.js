599242edd48e0f6e0cd60f4e95c4078748916ece
Jim Pick <jim@backblend.com>
2019-01-06 16:58:23 -0800

    Part 7 - Audio

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -72,6 +72,9 @@ game.PlayerEntity = me.Entity.extend({
         
               // set the jumping flag
               this.body.jumping = true;
+
+              // play some audio
+              me.audio.play("jump");
             }
         }
 
@@ -123,6 +126,9 @@ game.PlayerEntity = me.Entity.extend({
       
               // set the jumping flag
               this.body.jumping = true;
+
+              // play some audio
+              me.audio.play("stomp");
             }
             else {
               // let's flicker in case we touched an enemy
@@ -156,6 +162,9 @@ game.CoinEntity = me.CollectableEntity.extend({
     // an object is touched by something (here collected)
     onCollision : function (response, other) {
       // do something when collected
+
+      // play a "coin collected" sound
+      me.audio.play("cling");
 
       // give some score
       game.data.score += 250;
--- js/screens/play.js
+++ js/screens/play.js
@@ -6,6 +6,9 @@ game.PlayScreen = me.ScreenObject.extend({
         // load a level
         me.levelDirector.loadLevel("area01");
 
+        // play the audio track
+        me.audio.playTrack("dst-inertexponent");
+
         // reset the score
         game.data.score = 0;
 
@@ -13,12 +16,16 @@ game.PlayScreen = me.ScreenObject.extend({
         // Can also be forced by specifying a "Infinity" z value to the addChild function.
         this.HUD = new game.HUD.Container();
         me.game.world.addChild(this.HUD);
+
     },
 
     /**
      *  action to perform when leaving this screen (state change)
      */
     onDestroyEvent: function() {
+        // stop the current audio track
+        me.audio.stopTrack();
+
         // remove the HUD from the game world
         me.game.world.removeChild(this.HUD);
     }
